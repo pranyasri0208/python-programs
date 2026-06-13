6d9e9e2a3448fb81d65e7f506752eb99ca244d1c
pranyasri0208 <satyam.kamani@gmail.com>
2026-06-13 21:01:15 +0800

    welcome

---
+++ welcome to the world of coding.py
@@ -0,0 +1,10 @@
+#printing a sentence
+print("welcome to the world of programming!")
+#printing a number
+print(8)
+#printing next line after print
+print("hello world\n")
+#printing multiple arguments
+print("hello",2)
+#End argument of print statement
+print("welcome to ",end="*")
