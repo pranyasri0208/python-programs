acf83c2c94985a3cc3121c42fe022d3520cf14b5
pranyasri0208 <satyam.kamani@gmail.com>
2026-06-20 20:06:20 +0800

    storing birthdays

---
+++ storing birthdays.py
@@ -0,0 +1,11 @@
+
+printing my_birthdays list
+print(my_birthdays)
+print("my friends and family's birthdays are:")
+my_birthdays =[birthday1, birthday2, birthday3, birthday4, birthday5]
+birthday1 = "February 8, 2015"
+birthday2 = "March 28, 2011"
+birthday3 = "November 7, 1986"
+birthday4 = "June 12, 1979"
+birthday5 = "August 23, 2012"
+birthday6 = "April 22, 2009"
