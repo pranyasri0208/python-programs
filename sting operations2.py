ba40a75ee455d1de92b4e94f81b6befefc2119fe
pranyasri0208 <satyam.kamani@gmail.com>
2026-07-11 21:25:22 +0800

    string operations2

---
+++ sting operations2.py
@@ -0,0 +1,5 @@
+name = "coding"
+print("string:" , name)
+print("length of string is :", len(name))
+print("string in upper case is :", name.upper())
+print("concatenation of sting is :", name + " is interesting")
