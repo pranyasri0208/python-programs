fb068c9badcf3a1058f89c9416d7d697fb72e2a3
pranyasri0208 <satyam.kamani@gmail.com>
2026-07-11 21:02:02 +0800

    STRING operations

---
+++ STRING operartions.py
@@ -0,0 +1,9 @@
+#check the data type for the given string
+a = 5
+print("the type of a is:", type(a))
+b = 2.5
+print("the type of b is:", type(b))
+c = "hello"
+print("the type of c is:", type(c))
+d = False
+print("the type of d is:", type(d))
---
+++ data types.py
@@ -0,0 +1,4 @@
+book=4
+new_book=str(book)
+print(new_book)
+print(type(book))
