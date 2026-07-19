3323000346fd6ff0ca7f2e0ec7dee7ee6fe966d4
pranyasri0208 <satyam.kamani@gmail.com>
2026-07-19 10:17:49 +0800

    congratulations message

---
+++ congratulations message.py
@@ -0,0 +1,6 @@
+part1="Congratulations!\n"
+part2="You have done a great job!"
+part3="K.pranya\n"
+part4="keep up your good work and strive for excellence in all you do!"
+full_message = part1 + part2 + part3 + part4
+print(full_message)
