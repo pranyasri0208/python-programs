244446b228bacf4b142f3ef60f11056b27fe41b1
pranyasri0208 <satyam.kamani@gmail.com>
2026-07-19 12:46:17 +0800

    average

---
+++ average.py
@@ -0,0 +1,14 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+
+#finding the total of trees
+sum_of_trees=tree1+tree2+tree3+tree4+tree5
+print("sum of all the trees is:", sum_of_trees)
+
+#finding the average of trees
+average=sum_of_trees/5
+print("the average of trees is:", average )
