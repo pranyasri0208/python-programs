28041dc6f3c15ba9712d924cc21f32806d3f795a
pranyasri0208 <satyam.kamani@gmail.com>
2026-08-01 18:49:01 +0800

    square root

---
+++ SQUARE ROOT.py
@@ -0,0 +1,6 @@
+#define the number to find the square root of
+number=-2
+#raising the number to the power of 0.5 to find the square root
+square_root=number**0.5
+#printing the square root of the number
+print("the square root of", number, "is:", square_root)
