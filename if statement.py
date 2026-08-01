6917da2ecfaaeec42a846a5ce1350caedd7d815f
pranyasri0208 <satyam.kamani@gmail.com>
2026-08-01 21:03:47 +0800

    IF statement

---
+++ if statement.py
@@ -0,0 +1,8 @@
+#checking the number is positive or not
+num=int(input("enter a number:"))
+if num>0:
+    print("the number is positive")
+if num<0:
+    print("the number is negative")
+if num==0:
+    print("the number is zero")
