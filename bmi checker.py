c3f37c4d1a0b0d8383f76b4b054da07989e1eb44
pranyasri0208 <satyam.kamani@gmail.com>
2026-08-08 21:10:03 +0800

    BMI checker

---
+++ bmi checker.py
@@ -0,0 +1,16 @@
+height=float(input("please enter your height in cm: "))
+weight=float(input("please enter your weight in kg: "))
+BMI=weight/(height/100)**2
+print("your BMIis", BMI)
+if BMI<=18.4:
+    print("you are underweight")
+elif BMI<=24.9:
+    print("you are healthy")
+elif BMI<=29.9:
+    print("you are overweight")
+elif BMI<=34.9:
+    print("you are severely overweight")
+elif BMI<=39.9:
+    print("you are obese")
+else:
+    print("you are extremely obese")
