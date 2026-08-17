a3e92a73aa9252e46de34b17e0e052a8020455e1
pranyasri0208 <satyam.kamani@gmail.com>
2026-08-17 20:59:27 +0800

    and- or operator

---
+++ AND-OR operator.py
@@ -0,0 +1,14 @@
+a=2
+b=8
+c=10
+if a > b and b < c:
+    print("All variables are true")
+else:
+    print("At least one variable is false")
+a=7
+b=-6
+c=0
+if  a>0 or b>0 :
+    print("either of the number is greater than zero")
+else:
+    print("no numbers are greater than zero")
