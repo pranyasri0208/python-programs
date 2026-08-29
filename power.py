6ddb0f78c1511269ecaca7bf707f135f8093434a
pranyasri0208 <satyam.kamani@gmail.com>
2026-08-29 20:41:52 +0800

    power

---
+++ power.py
@@ -0,0 +1,7 @@
+num=int(input("Enter a whole number SO I CAN HELP U WITH UR HOMEWORK:" ))
+exponent=int(input("AAND YOU HAVE TO ENTER A POWER NUMBER TOO:"))
+result=1
+for i in range(exponent):
+    result*=num
+print("The result is:", result)
+print("yay! you used me to find out the answer!")
