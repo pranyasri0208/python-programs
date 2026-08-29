577ab4740e310fa61af7f896570e0d690652b42d
pranyasri0208 <satyam.kamani@gmail.com>
2026-08-29 20:54:18 +0800

    alphabet

---
+++ alphabets.py
@@ -0,0 +1,5 @@
+alpha=str(input("enter a alphabet...ONLY ONE ALPHABET:"))
+if alpha.isalpha():
+    print("yep! seems like you are not a baby! ")
+else:
+    print("nope! this aint a alphabet...try again")
